e178423a8612dde87fd4f251474c09495194f850
Paul Smith <Psmith4422@conestogac.on.ca>
2019-10-26 20:25:58 -0400

    starting anew

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -12,19 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Net.Sockets;
 
-namespace a05Client
+namespace Client
 {
-
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-
-            Int32 port = 13000;
-            
         }
     }
 }
